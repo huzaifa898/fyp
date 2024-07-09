ffc0f16a39d5f4215455d7de04d45d167c9147e4
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-07-09 21:24:54 +0500

    deplyoment changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import What from "./components/What";
-import Qoutes from "./components/Qoutes";
-import Work from "./components/Work";
-import Motivation from "./components/Motivation";
-import Footer from "./components/Footer";
+import {  Route, Routes } from 'react-router-dom';
 import Investors from "./components/Investors";
 import Entrepreneurs from "./components/Entrepreneurs";
 import Contact from "./components/Contact";
+import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
+    
+      
         <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <What />
-              <Qoutes />
-              <Work />
-              <Motivation />
-              <Footer />
-            </>
-          } />
+          <Route path="/" element={<Home/>} />
           <Route path="/investors" element={<Investors />} />
           <Route path="/entrepreneurs" element={<Entrepreneurs />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
-    </Router>
+     
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './Header'
+import Hero from './Hero'
+import What from './What'
+import Qoutes from './Qoutes'
+import Work from './Work'
+import Motivation from './Motivation'
+import Footer from './Footer'
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <What/>
+      <Qoutes/>
+      <Work/>
+      <Motivation/>
+      <Footer/>
+    </div>
+  )
+}
