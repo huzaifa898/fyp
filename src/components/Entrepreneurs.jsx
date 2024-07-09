975ba977edaf907b08b09c02eca2153feaeacdb3
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-07-10 02:45:17 +0500

    changes

--- src/components/Entrepreneurs.jsx
+++ src/components/Entrepreneurs.jsx
@@ -4,9 +4,11 @@ import Hero from './Hero';
 import Entrecommended from './Entrecomended';
 import Qoutes from './Qoutes'
 import Footer from './Footer';
+import Header from './Header';
 function Entrepreneurs() {
   return (
     <div>
+      <Header/>
       <Hero/>
       <Entrecommended/>
       <Qoutes/>
