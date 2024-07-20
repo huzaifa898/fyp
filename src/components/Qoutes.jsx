705ef4f8fb632f93af37952e5cd6b58caf8029f0
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-07-20 15:03:35 +0500

    updates qoutes section

--- src/components/Qoutes.jsx
+++ src/components/Qoutes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePageSection = () => {
   return (
     <section
-      className="relative h-screen bg-cover bg-center"
+      className="relative h-5/6 bg-cover bg-center"
       style={{
         backgroundImage: `url(https://cdn.pixabay.com/photo/2019/03/02/11/23/startup-4029671_1280.jpg)`,
       }}
@@ -11,7 +11,7 @@ const HomePageSection = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center text-center">
           
-          <button className="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold mt-56 py-3 px-6 font-abc rounded-full focus:outline-none focus:shadow-outline">
+          <button className="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold mt-56 mb-12 py-3 px-6 font-abc rounded-full focus:outline-none focus:shadow-outline">
             GET QUOTATION
           </button>
         </div>
