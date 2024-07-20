69f5496fd1cd3a13e153c07be6b974e0d402a332
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2024-07-20 13:20:08 +0500

    add mouse hover effect to what we do section

--- src/components/What.jsx
+++ src/components/What.jsx
@@ -7,7 +7,7 @@ const What = () => {
         <div className="text-center mb-16">
           <h2 className="text-4xl font-bold text-gray-800 font-abc">WHAT WE DO</h2>
           <h3 className="text-2xl font-medium text-gray-600 mt-2">
-           <span className='text-secondary font-cursive text-3xl'>EXPERTS</span> 
+            <span className="text-secondary font-cursive text-3xl">EXPERTS</span>
           </h3>
           <p className="text-lg text-black-500 font-bold mt-4 font-abc">
             Personalized Recommendations.
@@ -15,61 +15,103 @@ const What = () => {
           <div className="h-1 w-16 bg-gray-300 mx-auto mt-4"></div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-white-600 shadow-md rounded-lg p-8">
+          <div
+            className="bg-white-600 shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) => {
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               USER ENGAGEMENT AND INTERACTION
             </h4>
             <p className="text-black-500 leading-relaxed text-justify font-semibold font-abc">
-            Our platform is designed to foster meaningful connections between investors and entrepreneurs. We utilize intuitive user interfaces, personalized recommendations, and interactive features such as messaging and video calls to ensure a seamless and engaging user experience. By encouraging active participation and feedback, we aim to create a dynamic community where users can easily discover and connect with potential partners.
+              Our platform is designed to foster meaningful connections between investors and entrepreneurs. We utilize intuitive user interfaces, personalized recommendations, and interactive features such as messaging and video calls to ensure a seamless and engaging user experience. By encouraging active participation and feedback, we aim to create a dynamic community where users can easily discover and connect with potential partners.
             </p>
-            
           </div>
-          <div className="bg-white shadow-md rounded-lg p-8">
+          <div
+            className="bg-white shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) => {
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               DATA SECURITY AND PRIVACY
             </h4>
             <p className=" text-justify font-semibold font-abc">
-            We prioritize the privacy and security of our users' data. Our system implements robust encryption methods and adheres to strict data protection regulations to ensure that personal and sensitive information remains secure. Users have control over their data, with clear consent mechanisms and transparency about how their information is used and stored.
+              We prioritize the privacy and security of our users' data. Our system implements robust encryption methods and adheres to strict data protection regulations to ensure that personal and sensitive information remains secure. Users have control over their data, with clear consent mechanisms and transparency about how their information is used and stored.
             </p>
-            
           </div>
-          <div className="bg-white shadow-md rounded-lg p-8">
+          <div
+            className="bg-white shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) => {
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               ANALYTICS AND INSIGHTS
             </h4>
             <p className=" text-justify font-semibold font-abc">
-            Leveraging advanced analytics, our platform provides valuable insights to both investors and entrepreneurs. By analyzing user behavior, preferences, and interactions, we offer actionable recommendations and trends that help users make informed decisions. Our data-driven approach ensures that users receive the most relevant matches and opportunities, enhancing their overall experience.
+              Leveraging advanced analytics, our platform provides valuable insights to both investors and entrepreneurs. By analyzing user behavior, preferences, and interactions, we offer actionable recommendations and trends that help users make informed decisions. Our data-driven approach ensures that users receive the most relevant matches and opportunities, enhancing their overall experience.
             </p>
-            
           </div>
-          <div className="bg-white shadow-md rounded-lg p-8">
+          <div
+            className="bg-white shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) => {
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               USER SATISFACTION AND RETENTION
             </h4>
             <p className=" text-justify font-semibold font-abc">
-            User satisfaction is at the core of our mission. We continuously gather feedback and monitor user engagement to refine and improve our services. By addressing user needs and preferences promptly, we aim to build long-term relationships and ensure high levels of satisfaction and retention. Regular updates and enhancements keep our platform fresh and responsive to the evolving needs of our community.
+              User satisfaction is at the core of our mission. We continuously gather feedback and monitor user engagement to refine and improve our services. By addressing user needs and preferences promptly, we aim to build long-term relationships and ensure high levels of satisfaction and retention. Regular updates and enhancements keep our platform fresh and responsive to the evolving needs of our community.
             </p>
-            
           </div>
-          <div className="bg-white shadow-md rounded-lg p-8">
+          <div
+            className="bg-white shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) =>{
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               SCALABILITY AND PERFORMANCE
             </h4>
             <p className=" text-justify font-semibold font-abc">
-            Our platform is built to scale efficiently, accommodating a growing user base without compromising performance. We utilize cloud-based infrastructure and optimized algorithms to ensure fast and reliable service. Whether you're a new user or a long-time member, our system is designed to handle increasing demand and deliver a consistent, high-quality experience.
+              Our platform is built to scale efficiently, accommodating a growing user base without compromising performance. We utilize cloud-based infrastructure and optimized algorithms to ensure fast and reliable service. Whether you're a new user or a long-time member, our system is designed to handle increasing demand and deliver a consistent, high-quality experience.
             </p>
-            
           </div>
-          <div className="bg-white shadow-md rounded-lg p-8">
+          <div
+            className="bg-white shadow-md rounded-lg p-8 hover:bg-black transition duration-300 ease-in-out"
+            onMouseOver={(e) => {
+              e.target.style.color = "white";
+            }}
+            onMouseOut={(e) => {
+              e.target.style.color = "";
+            }}
+          >
             <h4 className="text-xl  mb-4 text-secondary font-cursive">
               PERSNOLIAZED RECOMMENDATION
             </h4>
             <p className=" text-justify font-semibold font-abc">
-            Our platform uses advanced machine learning to offer personalized recommendations for both investors and entrepreneurs. By analyzing profiles, preferences, and past interactions, we ensure users receive the most relevant connections. Entrepreneurs find the right investors for their ideas, and investors discover promising opportunities. This feature saves time and enhances the success rate of connections on our platform.
+              Our platform uses advanced machine learning to offer personalized recommendations for both investors and entrepreneurs. By analyzing profiles, preferences, and past interactions, we ensure users receive the most relevant connections. Entrepreneurs find the right investors for their ideas, and investors discover promising opportunities. This feature saves time and enhances the success rate of connections on our platform.
 
 
             </p>
-            
           </div>
         </div>
       </div>
