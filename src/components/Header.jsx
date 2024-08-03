fa57260f523853b53da4f6dbd4a864b49f19a068
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-08-03 22:44:02 +0500

    changes in header file .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,8 +27,8 @@ const Header = () => {
       <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
         {/* logo section */}
         <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-          <BookOpenIcon className='w-7 h-7 text-blue-600' />
-          <span className='font-abc'>IERS</span>
+          <BookOpenIcon className='w-7 h-7 text-green-600' />
+          <span className='font-montserrat'>IERS</span>
         </div>
         {/* Menu icon */}
         <div onClick={() => setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
@@ -37,14 +37,14 @@ const Header = () => {
         {/* link items */}
         <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
           {Links.map((link) => (
-            <li key={link.name} className='md:ml-8 md:my-0 my-7 font-bold font-abc'>
-              <NavLink to={link.link} className={({ isActive }) => isActive ? 'active-link' : 'text-gray-800 hover:text-blue-400 duration-500'}>
+            <li key={link.name} className='md:ml-8 md:my-0 my-7 font-bold font-sans'>
+              <NavLink to={link.link} className={({ isActive }) => isActive ? 'active-link' : 'text-gray-600 hover:text-green-600 duration-500'}>
                 {link.name}
               </NavLink>
             </li>
           ))}
           <button onClick={handleRegister} className='btn bg-green-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static font-abc'>Register</button>
-          <button onClick={handleLogin} className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static font-abc'>Login</button>
+          <button onClick={handleLogin} className='btn bg-coral-red text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static font-abc'>Login</button>
         </ul>
       </div>
     </div>
